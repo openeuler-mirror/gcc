1c69390a01d3bf7226fce2a670a0f71731744b04
huangxiaoquan <huangxiaoquan1@huawei.com>
2021-08-17 15:50:31 +0800

    [StructReorderFields] Fix pointer layer check bug
    In the pointer layer check, the NULL pointer check is added
    for the escape type mark.

---
+++ gcc/testsuite/gcc.dg/struct/rf_check_ptr_layers_bug.c
@@ -0,0 +1,24 @@
+/* check_ptr_layers bugfix.*/
+/* { dg-do compile } */
+struct {
+  char a;
+} **b = 0, *e = 0;
+long c;
+char d = 9;
+int f;
+
+void g()
+{
+  for (; f;)
+    if (c)
+      (*e).a++;
+  if (!d)
+    for (;;)
+      b &&c;
+}
+int
+main()
+{ 
+  g();
+}
+/* { dg-final { scan-ipa-dump "No structures to transform." "reorder_fields" } } */
