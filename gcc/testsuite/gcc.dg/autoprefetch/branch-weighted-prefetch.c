071d19832d788422034a3b052ff7ce91e1010344
dingguangya <dingguangya1@huawei.com>
2022-02-28 16:52:58 +0800

    [Autoprefetch] Prune invaild loops containing edges whose probability exceeds 1
    Skip auto prefetch analysis if the loop contains the bb in which the sum
    of its outgoing edge probabilities is greater than 1.

--- gcc/testsuite/gcc.dg/autoprefetch/branch-weighted-prefetch.c
+++ gcc/testsuite/gcc.dg/autoprefetch/branch-weighted-prefetch.c
@@ -1,5 +1,5 @@
-/* { dg-do compile } */
-/* { dg-options "-O2 -fprefetch-loop-arrays=2 --param min-insn-to-prefetch-ratio=5 --param simultaneous-prefetches=100 -fdump-tree-aprefetch-details -fdump-tree-optimized" } */
+/* { dg-do compile { target { aarch64*-*-linux* } } } */
+/* { dg-options "-O2 -fprefetch-loop-arrays=2 --param min-insn-to-prefetch-ratio=5 --param simultaneous-prefetches=100 --param l1-cache-size=64 --param l1-cache-line-size=32 -fdump-tree-aprefetch-details -fdump-tree-optimized" } */
 #define N 10000000
 
 long long a[N];
@@ -18,5 +18,5 @@ long long func ()
 
   return sum;
 }
-/* { dg-final { scan-tree-dump-times "Ahead 40" 1 "aprefetch" } } */
-/* { dg-final { scan-tree-dump-times "builtin_prefetch" 1 "optimized" } } */
+/* { dg-final { scan-tree-dump "Calculating prefetch distance using bb branch weighting method" "aprefetch" } } */
+/* { dg-final { scan-tree-dump "builtin_prefetch" "optimized" } } */
