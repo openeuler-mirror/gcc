60612cbd9cdd9b5079c0505b9d53c9cd98fba4b1
Kewen Lin <linkw@linux.ibm.com>
2022-11-15 20:26:07 -0600

    [Backport][SME] rtl: Try to remove EH edges after {pro,epi}logue generation [PR90259]
    Reference: https://gcc.gnu.org/git/?p=gcc.git;a=commit;h=63e1b2e767a3f4695373c2406ff719c0a60c1858
    
    After prologue and epilogue generation, the judgement on whether
    one memory access onto stack frame may trap or not could change,
    since we get more exact stack information by now.
    
    As PR90259 shows, some memory access becomes impossible to trap
    any more after prologue and epilogue generation, it can make
    subsequent optimization be able to remove it if safe, but it
    results in unexpected control flow status due to REG_EH_REGION
    note missing.
    
    This patch proposes to try to remove EH edges with function
    purge_all_dead_edges after prologue and epilogue generation,
    it simplifies CFG as early as we can and don't need any fixup
    in downstream passes.
    
    CFG simplification result with PR90259's case as example:
    
    *before*
    
       18: %1:TF=call [`__gcc_qdiv'] argc:0
          REG_EH_REGION 0x2
       77: NOTE_INSN_BASIC_BLOCK 3
       19: NOTE_INSN_DELETED
       20: NOTE_INSN_DELETED
      110: [%31:SI+0x20]=%1:DF
          REG_EH_REGION 0x2
      116: NOTE_INSN_BASIC_BLOCK 4
      111: [%31:SI+0x28]=%2:DF
          REG_EH_REGION 0x2
       22: NOTE_INSN_BASIC_BLOCK 5
      108: %0:DF=[%31:SI+0x20]
          REG_EH_REGION 0x2
      117: NOTE_INSN_BASIC_BLOCK 6
      109: %1:DF=[%31:SI+0x28]
          REG_EH_REGION 0x2
       79: NOTE_INSN_BASIC_BLOCK 7
       26: [%31:SI+0x18]=%0:DF
      104: pc=L69
      105: barrier
    
    *after*
    
       18: %1:TF=call [`__gcc_qdiv'] argc:0
          REG_EH_REGION 0x2
       77: NOTE_INSN_BASIC_BLOCK 3
       19: NOTE_INSN_DELETED
       20: NOTE_INSN_DELETED
      110: [%31:SI+0x20]=%1:DF
      111: [%31:SI+0x28]=%2:DF
      108: %0:DF=[%31:SI+0x20]
      109: %1:DF=[%31:SI+0x28]
       26: [%31:SI+0x18]=%0:DF
      104: pc=L69
      105: barrier
    
    	PR rtl-optimization/90259
    
    gcc/ChangeLog:
    
    	* function.cc (rest_of_handle_thread_prologue_and_epilogue): Add
    	parameter fun, and call function purge_all_dead_edges.
    	(pass_thread_prologue_and_epilogue::execute): Name unamed parameter
    	as fun, and use it for rest_of_handle_thread_prologue_and_epilogue.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.target/powerpc/pr90259.C: New.

--- gcc/function.cc
+++ gcc/function.cc
@@ -6529,7 +6529,7 @@ make_pass_leaf_regs (gcc::context *ctxt)
 }
 
 static unsigned int
-rest_of_handle_thread_prologue_and_epilogue (void)
+rest_of_handle_thread_prologue_and_epilogue (function *fun)
 {
   /* prepare_shrink_wrap is sensitive to the block structure of the control
      flow graph, so clean it up first.  */
@@ -6545,6 +6545,13 @@ rest_of_handle_thread_prologue_and_epilogue (void)
   /* Some non-cold blocks may now be only reachable from cold blocks.
      Fix that up.  */
   fixup_partitions ();
+
+  /* After prologue and epilogue generation, the judgement on whether
+     one memory access onto stack frame may trap or not could change,
+     since we get more exact stack information by now.  So try to
+     remove any EH edges here, see PR90259.  */
+  if (fun->can_throw_non_call_exceptions)
+    purge_all_dead_edges ();
 
   /* Shrink-wrapping can result in unreachable edges in the epilogue,
      see PR57320.  */
@@ -6614,9 +6621,9 @@ public:
   {}
 
   /* opt_pass methods: */
-  virtual unsigned int execute (function *)
+  unsigned int execute (function * fun) final override
     {
-      return rest_of_handle_thread_prologue_and_epilogue ();
+      return rest_of_handle_thread_prologue_and_epilogue (fun);
     }
 
 }; // class pass_thread_prologue_and_epilogue
