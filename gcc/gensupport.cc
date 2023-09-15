e593ad216bd1f4f75d9875898f352e0e5f978159
Andrea Corallo <andrea.corallo@arm.com>
2023-09-15 10:23:02 +0200

    [Backport][SME] recog: Support space in "[ cons"
    Reference: https://gcc.gnu.org/git/?p=gcc.git;a=commit;h=9d31045b21324166c3997d603961d99e3c4c357d
    
    Hi all,
    
    this is to allow for spaces before "cons:" in the definitions of
    patterns using the new compact syntax, ex:
    
    (define_insn "aarch64_simd_dup<mode>"
      [(set (match_operand:VDQ_I 0 "register_operand")
            (vec_duplicate:VDQ_I
              (match_operand:<VEL> 1 "register_operand")))]
      "TARGET_SIMD"
      {@ [ cons: =0 , 1  ; attrs: type      ]
         [ w        , w  ; neon_dup<q>      ] dup\t%0.<Vtype>, %1.<Vetype>[0]
         [ w        , ?r ; neon_from_gp<q>  ] dup\t%0.<Vtype>, %<vwcore>1
      }
    )
    
    gcc/Changelog
    
    2023-09-20  Andrea Corallo  <andrea.corallo@arm.com>
    
    	* gensupport.cc (convert_syntax): Skip spaces before "cons:"
    	in new compact pattern syntax.

--- gcc/gensupport.cc
+++ gcc/gensupport.cc
@@ -893,6 +893,8 @@ convert_syntax (rtx x, file_location loc)
   if (!expect_char (&templ, '['))
     fatal_at (loc, "expecing `[' to begin section list");
 
+  skip_spaces (&templ);
+
   parse_section_layout (loc, &templ, "cons:", tconvec, true);
 
   if (*templ != ']')
