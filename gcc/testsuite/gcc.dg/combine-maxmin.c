14a89260293f3c952acce1251c8687a86ca68dd1
Pronin Alexander <pronin.alexander@huawei.com>
2024-02-22 17:13:27 +0800

    [minmax] Port moving minmask pattern to gimple to GCC12.3.1
    Migrated from the commit in GCC10.3.1:
    https://gitee.com/openeuler/gcc/commit/df88d29c355c59e262397fdf3b22ee9099ce40c2

--- gcc/testsuite/gcc.dg/combine-maxmin.c
+++ gcc/testsuite/gcc.dg/combine-maxmin.c
@@ -1,5 +1,5 @@
 /* { dg-do compile { target aarch64-*-* } } */
-/* { dg-options "-O3 -fdump-rtl-combine-all" } */
+/* { dg-options "-O3 -fconvert-minmax" } */
 
 /* The test checks usage of smax/smin insns for clip evaluation and
  * uzp1/uzp2 insns for vector element narrowing.  It's inspired by
@@ -19,20 +19,26 @@ void hf (uint8_t *dsth, uint8_t *dstv, uint8_t *dstc, uint8_t *src,
 {
     const int pad = (8 > 9) ? (-10 * ((1 << 8)-1)) : 0;
     for( int y = 0; y < height; y++ ) {
+        /* This loop is not being vectorized now.  */
         for( int x = -2; x < width+3; x++ ) {
             int v = ((src)[x-2*stride] + (src)[x+3*stride] - 5*((src)[x-stride]
 		     + (src)[x+2*stride]) + 20*((src)[x] + (src)[x+stride]));
             dstv[x] = clip ( (v + 16) >> 5 );
             buf[x+2] = v + pad;
         }
+
+        /* Produces two versions of the code: 3xUZP1/2xMAX/2xMIN + 1xUZP1/1xMAX/1xMIN.  */
         for( int x = 0; x < width; x++ )
             dstc[x] = clip ((((buf+2)[x-2*1] + (buf+2)[x+3*1] - 5*((buf+2)[x-1]
 			      + (buf+2)[x+2*1]) + 20*((buf+2)[x] + (buf+2)[x+1]))
 			     - 32*pad + 512) >> 10);
+
+        /* Priduces two versions of the code: 1xUZP1/2xMAX/2xMIN + 0xUZP1/1xMAX/1xMIN.  */
         for( int x = 0; x < width; x++ )
             dsth[x] = clip ((((src)[x-2*1] + (src)[x+3*1] - 5*((src)[x-1]
 			      + (src)[x+2*1]) + 20*((src)[x] + (src)[x+1]))
 			     + 16) >> 5);
+
         dsth += stride;
         dstv += stride;
         dstc += stride;
@@ -40,7 +46,10 @@ void hf (uint8_t *dsth, uint8_t *dstv, uint8_t *dstc, uint8_t *src,
     }
 }
 
-/* { dg-final { scan-assembler-times {smax\t} 4 } }  */
-/* { dg-final { scan-assembler-times {smin\t} 4 } }  */
-/* { dg-final { scan-assembler-times {cmtst\t} 2 } }  */
-/* { dg-final { scan-assembler-times {uzp1\t} 6 } }  */
+/* Max is performed on 0 from signed values, match smax exactly.  */
+/* { dg-final { scan-assembler-times {smax\t} 6 } }  */
+/* Min is performed on signed val>0 and a mask, min sign doesn't matter.  */
+/* { dg-final { scan-assembler-times {[us]min\t} 6 } }  */
+/* All of the vectorized patterns are expected to be matched.  */
+/* { dg-final { scan-assembler-not {cmtst\t} } }  */
+/* { dg-final { scan-assembler-times {uzp1\t} 5 } }  */
