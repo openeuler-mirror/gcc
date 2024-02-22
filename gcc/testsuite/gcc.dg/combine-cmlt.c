15e74c5d7ef5ce143c8c6580416864399d819789
Diachkov Ilia <diachkov.ilia1@huawei-partners.com>
2024-02-22 11:27:43 +0300

    Port cmlt_arith to GCC12.3.1
    Add insn defs and correct costs for cmlt generation
    
    Migrated from the commit in GCC 10.3.1:
    https://gitee.com/openeuler/gcc/commit/80b7de670da46d8921118799904cba4a0753bb72

---
+++ gcc/testsuite/gcc.dg/combine-cmlt.c
@@ -0,0 +1,20 @@
+/* { dg-do compile { target aarch64-*-* } } */
+/* { dg-options "-O3 -mcmlt-arith" } */
+
+/* The test checks usage of cmlt insns for arithmetic/logic calculations
+ * in foo ().  It's inspired by sources of x264 codec.  */
+
+typedef unsigned short int uint16_t;
+typedef unsigned int uint32_t;
+
+void foo( uint32_t *a, uint32_t *b)
+{
+  for (unsigned i = 0; i < 4; i++)
+    {
+      uint32_t s = ((a[i]>>((8 * sizeof(uint16_t))-1))
+		    &(((uint32_t)1<<(8 * sizeof(uint16_t)))+1))*((uint16_t)-1);
+      b[i] = (a[i]+s)^s;
+    }
+}
+
+/* { dg-final { scan-assembler-times {cmlt\t} 1 } }  */
