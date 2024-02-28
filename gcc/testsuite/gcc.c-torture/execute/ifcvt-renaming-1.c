3159fc92f3078591db0821dc92abb921b86eff21
vchernon <chernonog.vyacheslav@huawei.com>
2024-02-28 23:05:12 +0800

    introduce rtl ifcvt enchancements
        new option:
          -fifcvt-allow-complicated-cmps:
            allows ifcvt to deal with complicated cmps like
    
            cmp reg1 (reg2 + reg3)
    
            can increase compilation time
        new param:
          -param=ifcvt-allow-register-renaming=[0,1,2]
            1 : allows ifcvt to rename registers in then and else bb
            2 : allows to rename registers in condition and else/then bb
            can increase compilation time and register pressure

---
+++ gcc/testsuite/gcc.c-torture/execute/ifcvt-renaming-1.c
@@ -0,0 +1,35 @@
+
+extern void abort(void);
+
+__attribute__ ((noinline))
+int foo (int x, int y, int z, int a, int b)
+{
+  if (a < 2) {
+      if (a == 0) {
+	  if (x - y < 0)
+	    x = x - y + z;
+	  else
+	    x = x - y;
+	}
+      else {
+	  if (x + y >= z)
+	    x = x + y - z;
+	  else
+	    x = x + y;
+	}
+    }
+  return x;
+}
+
+int main(void) {
+  if (foo (5,10,7,0,1) != 2) // x - y + z = -5 + 7 = 2
+    abort ();
+  if (foo (50,10,7,0,1) != 40) // x - y = 40
+    abort ();
+  if (foo (5,10,7,1,1) != 8) // x + y - z = 5 + 10 - 7 = 8
+    abort ();
+  if (foo (5,10,70,1,1) != 15) // x + y = 15
+    abort ();
+  return 0;
+}
+
