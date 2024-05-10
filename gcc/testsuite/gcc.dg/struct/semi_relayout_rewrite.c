6f53eb333d356beea0aa724d0b1ee0c9a43d9baa
tiancheng-bao <baotiancheng1@huawei.com>
2024-05-10 17:52:27 +0800

    fix bugs within pointer compression and DFE

--- gcc/testsuite/gcc.dg/struct/semi_relayout_rewrite.c
+++ gcc/testsuite/gcc.dg/struct/semi_relayout_rewrite.c
@@ -76,6 +76,7 @@ main ()
       node->potential = i + 1;
       arc->cost = arc->head->potential;
       arc->tail = node->sibling;
+      arc->head = arc->tail;
       node = node + 1;
       arc = arc + 1;
     }
