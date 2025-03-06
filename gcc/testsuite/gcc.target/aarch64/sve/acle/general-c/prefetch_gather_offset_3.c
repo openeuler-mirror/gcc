c7bdc03e48a0b6e213c5a4b8c821665d7ca897bb
陈鸿 <chenhong92@huawei.com>
2025-03-06 14:58:57 +0800

    fix prefetch case failed

--- gcc/testsuite/gcc.target/aarch64/sve/acle/general-c/prefetch_gather_offset_3.c
+++ gcc/testsuite/gcc.target/aarch64/sve/acle/general-c/prefetch_gather_offset_3.c
@@ -10,8 +10,8 @@ f1 (svbool_t pg, int32_t *s32_ptr, svint32_t s32, enum svprfop op)
   svprfb_gather_s32offset (pg, s32_ptr, s32, (enum svprfop) -1); /* { dg-error {passing 4294967295 to argument 4 of 'svprfb_gather_s32offset', which expects a valid 'enum svprfop' value} } */
   svprfb_gather_s32offset (pg, s32_ptr, s32, (enum svprfop) 0);
   svprfb_gather_s32offset (pg, s32_ptr, s32, (enum svprfop) 5);
-  svprfb_gather_s32offset (pg, s32_ptr, s32, (enum svprfop) 6); /* { dg-error {passing 6 to argument 4 of 'svprfb_gather_s32offset', which expects a valid 'enum svprfop' value} } */
-  svprfb_gather_s32offset (pg, s32_ptr, s32, (enum svprfop) 7); /* { dg-error {passing 7 to argument 4 of 'svprfb_gather_s32offset', which expects a valid 'enum svprfop' value} } */
+  svprfb_gather_s32offset (pg, s32_ptr, s32, (enum svprfop) 6);
+  svprfb_gather_s32offset (pg, s32_ptr, s32, (enum svprfop) 7);
   svprfb_gather_s32offset (pg, s32_ptr, s32, (enum svprfop) 8);
-  svprfb_gather_s32offset (pg, s32_ptr, s32, (enum svprfop) 14); /* { dg-error {passing 14 to argument 4 of 'svprfb_gather_s32offset', which expects a valid 'enum svprfop' value} } */
+  svprfb_gather_s32offset (pg, s32_ptr, s32, (enum svprfop) 14);
 }
