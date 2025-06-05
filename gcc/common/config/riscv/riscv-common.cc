f1d5231b20032ab09d104df72443cda34c482193
Jiawei <jiawei@iscas.ac.cn>
2025-06-05 13:59:14 +0800

    RISC-V: Support Ssu64xl extension.
    Support the Ssu64xl extension, which requires UXLEN to be 64.
    
    gcc/ChangeLog:
    
    	* config/riscv/riscv-ext.def: New extension definition.
    	* config/riscv/riscv-ext.opt: New extension mask.
    	* doc/riscv-ext.texi: Document the new extension.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/arch-ssu64xl.c: New test.
    
    Signed-off-by: Jiawei <jiawei@iscas.ac.cn>
    (cherry picked from commit 8e1f06d1d1c8bb25c44c321f90c39f36eb18f344)

--- gcc/common/config/riscv/riscv-common.cc
+++ gcc/common/config/riscv/riscv-common.cc
@@ -234,6 +234,7 @@ static const riscv_implied_info_t riscv_implied_info[] =
   {"sstc", "zicsr"},
   {"sstvala", "zicsr"},
   {"sstvecd", "zicsr"},
+  {"ssu64xl", "zicsr"},
   {"svadu", "zicsr"},
   {"svade", "zicsr"},
 
@@ -427,6 +428,7 @@ static const struct riscv_ext_version riscv_ext_version_table[] =
   {"sstc",      ISA_SPEC_CLASS_NONE, 1, 0},
   {"sstvala",   ISA_SPEC_CLASS_NONE, 1, 0},
   {"sstvecd",   ISA_SPEC_CLASS_NONE, 1, 0},
+  {"ssu64xl",   ISA_SPEC_CLASS_NONE, 1, 0},
 
   {"svade",   ISA_SPEC_CLASS_NONE, 1, 0},
   {"svadu",   ISA_SPEC_CLASS_NONE, 1, 0},
