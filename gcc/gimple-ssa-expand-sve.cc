c6f5176674fe1796f6eecc487581f7fba49baec8
cyf <1182564018@qq.com>
2026-04-28 14:46:24 +0800

    bugfix for find-with-sve

--- gcc/gimple-ssa-expand-sve.cc
+++ gcc/gimple-ssa-expand-sve.cc
@@ -134,6 +134,61 @@ private:
   uint8_t bit_width;
   const char *null_name = "";
 
+  bool find_element_64bit_type_p (tree type)
+  {
+    if (type == nullptr)
+      return false;
+
+    type = TYPE_MAIN_VARIANT (type);
+    if (TREE_CODE (type) == INTEGER_TYPE)
+      return TYPE_PRECISION (type) == 64;
+
+    if (TREE_CODE (type) != POINTER_TYPE)
+      return false;
+
+    tree size = TYPE_SIZE (type);
+    return size && tree_fits_uhwi_p (size) && tree_to_uhwi (size) == 64;
+  }
+
+  bool pointer_to_64bit_find_element_p (tree type)
+  {
+    if (type == nullptr || TREE_CODE (type) != POINTER_TYPE)
+      return false;
+
+    return find_element_64bit_type_p (TREE_TYPE (type));
+  }
+
+  tree normal_iterator_pointer_type (tree type)
+  {
+    if (type == nullptr || TREE_CODE (type) != RECORD_TYPE)
+      return nullptr;
+
+    for (tree field = TYPE_FIELDS (type); field; field = DECL_CHAIN (field))
+    {
+      if (TREE_CODE (field) != FIELD_DECL)
+	continue;
+
+      tree field_type = TREE_TYPE (field);
+      if (field_type && TREE_CODE (field_type) == POINTER_TYPE)
+	return field_type;
+    }
+
+    return nullptr;
+  }
+
+  bool record_pointer_type_p (tree type)
+  {
+    if (type == nullptr || TREE_CODE (type) != POINTER_TYPE)
+      return false;
+
+    tree pointed_type = TREE_TYPE (type);
+    if (pointed_type == nullptr)
+      return false;
+
+    pointed_type = TYPE_MAIN_VARIANT (pointed_type);
+    return TREE_CODE (pointed_type) == RECORD_TYPE;
+  }
+
   bool std_find_check (gimple *stmt)
   {
     if (!is_gimple_call (stmt))
@@ -149,6 +204,9 @@ private:
 
     if (DECL_CONTEXT (fndecl) == nullptr
       || TREE_CODE (DECL_CONTEXT (fndecl)) != NAMESPACE_DECL)
+      return false;
+
+    if (DECL_NAME (DECL_CONTEXT (fndecl)) == nullptr)
       return false;
 
     const char *namespace_name
@@ -186,19 +244,32 @@ private:
 	return false;
 
       this->bit_width = 64;
-    } else if (TREE_CODE (main_type) == POINTER_TYPE)
+    } else if (TREE_CODE (main_type) == POINTER_TYPE) {
+      if (record_pointer_type_p (main_type))
+	return false;
       this->bit_width = 64;
+    }
+
     else
       return false;
 
     tree arg1_type = TREE_TYPE (arg1);
     if (TREE_CODE (arg1_type) == POINTER_TYPE)
+    {
+      if (!pointer_to_64bit_find_element_p (arg1_type))
+	      return false;
       return true;
+    }
     else if (TREE_CODE (arg1_type) == RECORD_TYPE)
     {
       const char *type_name = get_type_name_arg (arg1_type);
       if (strcmp (type_name, "__normal_iterator") == 0)
-	return true;
+      {
+	      tree arg1_pointer_type = normal_iterator_pointer_type (arg1_type);
+	      if (!pointer_to_64bit_find_element_p (arg1_pointer_type))
+	        return false;
+	      return true;
+      }
     }
 
     return false;
