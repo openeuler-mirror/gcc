f0123de538936cc94f4b6613b79da94f513784b3
Cutie Deng <cutiedeng@icloud.com>
2026-05-09 09:47:49 +0800

    (TEST "Add aarch64 schoolbook-widen tests"
    "
    Add two dg-compile tests for the aarch64 schoolbook-widen pass: a
    positive test verifying the pass rewrites a 64x64->128 schoolbook
    multiply (expecting one umulh) and a negative test ensuring the pass
    bails out when an intermediate hh value has an extra use.
    
    Modified:
    - gcc/testsuite/gcc.target/aarch64/schoolbook-widen-1.c
    - gcc/testsuite/gcc.target/aarch64/schoolbook-widen-negative-1.c
    ")

---
+++ gcc/testsuite/gcc.target/aarch64/schoolbook-widen-1.c
@@ -0,0 +1,28 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -mschoolbook-widen -fdump-tree-schoolbook_widen-details" } */
+
+typedef unsigned long long uint64_t;
+
+void __attribute__((noipa))
+schoolbook_mul64 (uint64_t x, uint64_t y, uint64_t *hi, uint64_t *lo)
+{
+  const uint64_t mask = 0xffffffffULL;
+  uint64_t x_lo = x & mask;
+  uint64_t x_hi = x >> 32;
+  uint64_t y_lo = y & mask;
+  uint64_t y_hi = y >> 32;
+
+  uint64_t ll = x_lo * y_lo;
+  uint64_t lh = x_lo * y_hi;
+  uint64_t hl = x_hi * y_lo;
+  uint64_t hh = x_hi * y_hi;
+  uint64_t mid = lh + hl;
+  uint64_t carry = (uint64_t) (hl > mid) << 32;
+  uint64_t temp = (ll >> 32) + (mid & mask);
+
+  *lo = (temp << 32) | (ll & mask);
+  *hi = hh + (mid >> 32) + (temp >> 32) + carry;
+}
+
+/* { dg-final { scan-tree-dump-times "schoolbook_widen: rewrote schoolbook" 1 "schoolbook_widen" } } */
+/* { dg-final { scan-assembler-times {\tumulh\t} 1 } } */
