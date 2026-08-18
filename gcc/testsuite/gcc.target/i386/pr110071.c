dc400f11c50747f79b7409ff654ccded2d5de654
Zhongjie Guo <guozhongjie@hygon.cn>
2026-08-18 16:35:03 +0800

    [Testsuite] Add x86 test for PR rtl-optimization/110071
    The upstream test is restricted to powerpc and never runs on openEuler
    targets.  Add an i386 variant verified to pass with the backported IRA
    callee-save cost fix.
    
    Signed-off-by: Zhongjie Guo <guozhongjie@hygon.cn>

---
+++ gcc/testsuite/gcc.target/i386/pr110071.c
@@ -0,0 +1,16 @@
+/* { dg-do compile { target { ! ia32 } } } */
+/* { dg-options "-O2 -fdump-rtl-pro_and_epilogue" } */
+
+/* Verify there is an early return without the prolog and shrink-wrap
+   the function. */
+
+void bar ();
+long
+foo (long i, long cond)
+{
+  if (cond)
+    bar ();
+  return i+1;
+}
+
+/* { dg-final { scan-rtl-dump-times "Performing shrink-wrapping" 1 "pro_and_epilogue" } } */
